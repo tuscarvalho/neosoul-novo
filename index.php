ebf4637ed12d1987dceebbfad947301df462c88c
tuscarvalho <tiago.carvalho.sitel@gmail.com>
2017-04-05 11:44:50 -0300

    Criação da Index sem os estilos e efeitos

---
+++ index.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Digital Agency Landing Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bundle.css">
+</head>
+
+<body>
+<!--Header-->
+<header>
+    <div class="top-bar">
+        <div class="container">
+            <div class="col-md-2 col-sm-3 phone">
+                <i class="fa fa-phone" aria-hidden="true"></i> <a>+805345899404</a>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:name@email.com">info@domainname.com</a>
+            </div>
+            <div class="col-md-3 col-md-offset-4 login">
+                <a href="#"><i class="fa fa-user-o" aria-hidden="true"></i>Login - Sign in</a>
+            </div>
+        </div>
+    </div>
+    <nav>
+        <div class="container">
+            <div class="col-md-3 col-sm-6 col-xs-6 logo">
+                <img src="images/logo.png" alt="">
+            </div>
+            <div class="col-md-9 col-sm-6 col-xs-6">
+                <ul class="menu right">
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#pricing">Pricing</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+                <!--Mobile menu button-->
+                <a href="#" data-activates="slide-out" class="button-collapse hide-on-large-only">
+                    <i class="fa fa-bars" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+    </nav>
+    <ul id="slide-out" class="side-nav"></ul>
+    <div class="container content">
+        <div class="content">
+            <h1>Digital Agency</h1>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing ealit, sed diaim nonummy nibsih euismod tincidiunt laorieet doloire magna diam aliquafm erat voluitpati.</p>
+            <a class="btn dark-blue waves-effect waves-light">Sign Up</a>
+        </div>
+    </div>
+</header>
+<!--Header End-->
+<main>
+    <!--About-->
+    <section class="about" id="about">
+        <h3>About Us</h3>
+        <div class="container">
+            <div class="col-md-3 col-sm-6">
+                <h5 id="hourglass">0</h5>
+                <span class="icon"><i class="fa fa-hourglass-half" aria-hidden="true"></i></span>
+                <p>Lorem ipsum
+                    <br> dolar sit amet</p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h5 id="chart">0</h5>
+                <span class="icon"><i class="fa fa-line-chart" aria-hidden="true"></i></span>
+                <p>Lorem ipsum
+                    <br> dolar sit amet</p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h5 id="usd">0</h5>
+                <span class="icon"><i class="fa fa-usd" aria-hidden="true"></i></span>
+                <p>Lorem ipsum
+                    <br> dolar sit amet</p>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h5 id="checked">0</h5>
+                <span class="icon"><i class="fa fa-check-square-o" aria-hidden="true"></i></span>
+                <p>Lorem ipsum
+                    <br> dolar sit amet</p>
+            </div>
+        </div>
+    </section>
+    <!--About End-->
+    <!--Services-->
+    <section class="services" id="services">
+        <div class="container">
+            <h3>Services</h3>
+            <div class="col-md-4 col-sm-6">
+                <div class="card-panel hoverable waves-effect">
+                    <div class="service-title">
+                        <span><i class="fa fa-wordpress" aria-hidden="true"></i></span>
+                        <h4>Wordpress</h4>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing ealit, sed diaim nonummy nibsih euismod tincidiunt laorieet doloire magna diam aliquafm erat voluitpati.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="card-panel hoverable waves-effect">
+                    <div class="service-title">
+                        <span><i class="fa fa-paint-brush" aria-hidden="true"></i></span>
+                        <h4>Web Design</h4>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing ealit, sed diaim nonummy nibsih euismod tincidiunt laorieet doloire magna diam aliquafm erat voluitpati.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="card-panel hoverable waves-effect">
+                    <div class="service-title">
+                        <span><i class="fa fa-desktop" aria-hidden="true"></i></span>
+                        <h4>Responsive</h4>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing ealit, sed diaim nonummy nibsih euismod tincidiunt laorieet doloire magna diam aliquafm erat voluitpati.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="card-panel hoverable waves-effect">
+                    <div class="service-title">
+                        <span><i class="fa fa-mobile" aria-hidden="true"></i></span>
+                        <h4>Mobile App</h4>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing ealit, sed diaim nonummy nibsih euismod tincidiunt laorieet doloire magna diam aliquafm erat voluitpati.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="card-panel hoverable waves-effect">
+                    <div class="service-title">
+                        <span><i class="fa fa-line-chart" aria-hidden="true"></i></span>
+                        <h4>Marketing</h4>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing ealit, sed diaim nonummy nibsih euismod tincidiunt laorieet doloire magna diam aliquafm erat voluitpati.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="card-panel hoverable waves-effect">
+                    <div class="service-title">
+                        <span><i class="fa fa-life-ring" aria-hidden="true"></i></span>
+                        <h4>Support</h4>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing ealit, sed diaim nonummy nibsih euismod tincidiunt laorieet doloire magna diam aliquafm erat voluitpati.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Services End-->
+    <!--Portfolio-->
+    <section class="portfolio" id="portfolio">
+        <h3>Portfolio</h3>
+        <div class="container">
+            <div class="col-md-4 col-sm-6">
+                <img src="images/portfolio-1.jpg" alt="" class="materialboxed">
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <img src="images/portfolio-2.jpg" alt="" class="materialboxed">
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <img src="images/portfolio-3.jpg" alt="" class="materialboxed">
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <img src="images/portfolio-4.jpg" alt="" class="materialboxed">
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <img src="images/portfolio-5.jpg" alt="" class="materialboxed">
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <img src="images/portfolio-6.jpg" alt="" class="materialboxed">
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <img src="images/portfolio-7.jpg" alt="" class="materialboxed">
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <img src="images/portfolio-8.jpg" alt="" class="materialboxed">
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <img src="images/portfolio-9.jpg" alt="" class="materialboxed">
+            </div>
+        </div>
+    </section>
+    <!--Portfolio End-->
+    <!--Skills-->
+    <section class="skills" id="skills">
+        <h3>Skills</h3>
+        <div class="container">
+            <div class="card-panel">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="c100 big p65">
+                            <span>65%</span>
+                            <div class="slice">
+                                <div class="bar"></div>
+                                <div class="fill"></div>
+                            </div>
+                        </div>
+                        <p class="center-align">HTML &amp; CSS</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="c100 big p65">
+                            <span>65%</span>
+                            <div class="slice">
+                                <div class="bar"></div>
+                                <div class="fill"></div>
+                            </div>
+                        </div>
+                        <p class="center-align">JAVASCRIPT</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="c100 big p85">
+                            <span>85%</span>
+                            <div class="slice">
+                                <div class="bar"></div>
+                                <div class="fill"></div>
+                            </div>
+                        </div>
+                        <p class="center-align">BOOTSTRAP</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="c100 big p50">
+                            <span>50%</span>
+                            <div class="slice">
+                                <div class="bar"></div>
+                                <div class="fill"></div>
+                            </div>
+                        </div>
+                        <p class="center-align">WORDPRESS</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="c100 big p70">
+                            <span>70%</span>
+                            <div class="slice">
+                                <div class="bar"></div>
+                                <div class="fill"></div>
+                            </div>
+                        </div>
+                        <p class="center-align">SQL</p>
+                    </div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="c100 big p90">
+                            <span>90%</span>
+                            <div class="slice">
+                                <div class="bar"></div>
+                                <div class="fill"></div>
+                            </div>
+                        </div>
+                        <p class="center-align">PHOTOSHOP</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Skills End-->
+    <!--Pricing-->
+    <section class="pricing" id="pricing">
+        <h3>Pricing</h3>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-4">
+                    <div class="pricing-table z-depth-2 bg-color-light">
+                        <div class="head">Free</div>
+                        <div class="price">$0</div>
+                        <ul>
+                            <li>3 Users</li>
+                            <li>2 Workspaces</li>
+                            <li>5 Clients</li>
+                            <li>1gb Storage</li>
+                        </ul>
+                        <a class="btn bg-color waves-effect">Sign Up</a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="pricing-table z-depth-2 bg-color-light">
+                        <div class="head">Basic</div>
+                        <div class="price">$20</div>
+                        <ul>
+                            <li>3 Users</li>
+                            <li>2 Workspaces</li>
+                            <li>5 Clients</li>
+                            <li>1gb Storage</li>
+                        </ul>
+                        <a class="btn bg-color waves-effect">Sign Up</a>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="pricing-table z-depth-2 bg-color-light">
+                        <div class="head">Premium</div>
+                        <div class="price">$100</div>
+                        <ul>
+                            <li>3 Users</li>
+                            <li>2 Workspaces</li>
+                            <li>5 Clients</li>
+                            <li>1gb Storage</li>
+                        </ul>
+                        <a class="btn bg-color waves-effect">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Pricing End-->
+    <!--Testimonial-->
+    <section class="testimonial" id="testimonial">
+        <h3>Clients</h3>
+        <div class="swiper-container testimonials-container container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide waves-effect card-panel">
+                    <div class="col-md-3 col-sm-3 col-xs-3">
+                        <img src="images/team-1.jpg" alt="" class="circle responsive-img">
+                    </div>
+                    <div class="col-md-7 col-sm-6 col-xs-6">
+                        <h5>John Martin</h5>
+                        <p class="star"><span>&#9733;</span> <span>&#9733;</span> <span>&#9733;</span> <span>&#9733;</span> <span>&#9733;</span></p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu tellus ultrices, mattis ipsum in, pretium eros.</p>
+                    </div>
+                </div>
+                <div class="swiper-slide waves-effect card-panel">
+                    <div class="col-md-3 col-sm-3 col-xs-3">
+                        <img src="images/team-1.jpg" alt="" class="circle responsive-img">
+                    </div>
+                    <div class="col-md-7 col-sm-6 col-xs-6">
+                        <h5>John Martin</h5>
+                        <p class="star"><span>&#9733;</span> <span>&#9733;</span> <span>&#9733;</span> <span>&#9733;</span> <span>&#9733;</span></p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu tellus ultrices, mattis ipsum in, pretium eros.</p>
+                    </div>
+                </div>
+                <div class="swiper-slide waves-effect card-panel">
+                    <div class="col-md-3 col-sm-3 col-xs-3">
+                        <img src="images/team-1.jpg" alt="" class="circle responsive-img">
+                    </div>
+                    <div class="col-md-7 col-sm-6 col-xs-6">
+                        <h5>John Martin</h5>
+                        <p class="star"><span>&#9733;</span> <span>&#9733;</span> <span>&#9733;</span> <span>&#9733;</span> <span>&#9733;</span></p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu tellus ultrices, mattis ipsum in, pretium eros.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+        </div>
+    </section>
+    <!--Testimonial End-->
+    <!--Contact-->
+    <section class="contact" id="contact">
+        <h3>Contact</h3>
+        <div class="container">
+            <div>
+                <div class="input-field col-md-4">
+                    <input id="first_name" type="text" class="validate">
+                    <label for="first_name">First Name</label>
+                </div>
+                <div class="input-field col-md-4">
+                    <input id="mail" type="email" class="validate">
+                    <label for="mail">E-Mail</label>
+                </div>
+                <div class="input-field col-md-4">
+                    <input id="subject" type="text" class="validate">
+                    <label for="subject">Subject</label>
+                </div>
+            </div>
+            <div>
+                <div class="input-field col-md-12">
+                    <textarea id="message" class="materialize-textarea"></textarea>
+                    <label for="message">Message</label>
+                </div>
+            </div>
+            <span><a class="btn bg-color waves-effect">Submit</a></span>
+        </div>
+    </section>
+    <!--Contact End-->
+    <!--Footer-->
+    <footer>
+        <div class="container">
+            <div class="col-md-2 logo">
+                <img src="images/logo.png" alt="">
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                <div class="clear"></div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>+90 538 129 67 45 <br> +90 533 362 95 03</span>
+                <div class="clear"></div>
+            </div>
+            <div class="col-md-4 col-sm-12 social-media">
+                <a class="btn-floating"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a class="btn-floating"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a class="btn-floating"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                <a class="btn-floating"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                <a class="btn-floating"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </footer>
+    <!--Footer End-->
+</main>
+
+<script src="js/bundle.js"></script>
+<script src="js/app.js"></script>
+</body>
+
+</html>
